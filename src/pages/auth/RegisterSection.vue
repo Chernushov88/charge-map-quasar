fc7287bc803ab9b18c0020db2bfa4a3bb805bae1
chernushov88@gmail.com <chernushov88@gmail.com>
2023-04-08 23:21:09 +0100

    pages for project structure

---
+++ src/pages/auth/RegisterSection.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+
+</script>
+
+<style lang="scss">
+
+</style>
